7a9bf58f1b664541d6c37293c5ba2522f038381f
Mika Turunen <mika.turunen@ymail.com>
2017-05-23 06:32:49 +0300

    Update generator files.

--- api/generate-files-from-templates.sh
+++ api/generate-files-from-templates.sh
@@ -2,10 +2,19 @@
 
 TEMPLATES="./docker-templates"
 PAYMENTS="./payments"
+MERCHANT="./merchant"
 
 # Create the Dockerfile from node dockerfile base template for payments
 cat $TEMPLATES/Dockerfile-node-shared $PAYMENTS/Dockerfile-payments > $PAYMENTS/Dockerfile
 echo "Generated Dockerfile for payments API."
+# Copies the node shared configuration for the API
+cat node-api.yml > $PAYMENTS/source/config/app.yml
+echo "Copied node-api.yml to payments API config/app.yml"
+
+cat $TEMPLATES/Dockerfile-node-shared $MERCHANT/Dockerfile-merchant > $MERCHANT/Dockerfile
+echo "Generated Dockerfile for payments API."
+cat node-api.yml > $MERCHANT/source/config/app.yml
+echo "Copied node-api.yml to merchant API config/app.yml"
 
 # Create production ready docker-compose.yml
 cat $TEMPLATES/docker-compose-shared.yml $PAYMENTS/docker-compose-payments.yml > docker-compose.yml
