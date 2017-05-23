df3c9c39fd05af03bfce1b45a29382b633e77661
Mika Turunen <mika.turunen@ymail.com>
2017-05-23 20:18:33 +0300

    Mock db connection usage

--- api/merchant/source/app.js
+++ api/merchant/source/app.js
@@ -4,18 +4,26 @@ const config = require('konfig')({ path: path.join(__dirname, 'config') })
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-const redis = require('redis')
+const knex = require('knex')
 
 app.use(bodyParser.json())
 
-/**
- * Fetches Merchant.
- * @param {Express.Request} request
- * @param {Express.Response} response
- **/
-app.get('/merchant/:merchantId', (request, response) => {
-
+const database = knex({
+  client: 'pg',
+  debug: process.env.NODE_ENV !== 'production',
+  connection: {
+    host: config.app.postgresHost,
+    port: config.app.postgresPort,
+    user: config.app.postgresUser,
+    password: config.app.postgresPassword,
+    database: config.app.postgresDatabase
+  },
+  pool: {
+    min: config.app.postgresPoolMin,
+    max: config.app.postgresPoolMax,
+  }
 })
+
 
 /**
  * Fetches Merchant key to use in communication.
@@ -23,7 +31,25 @@ app.get('/merchant/:merchantId', (request, response) => {
  * @param {Express.Response} response
  **/
 app.get('/merchant/:merchantId', (request, response) => {
+  const merchant_id = request.params.merchantId
+  database
+      .select()
+      .from('merchant_link')
+      .where({
+        merchant_id
+      })
+    .then(result => {
+      response.json(result)
+    })
+    .catch(error => {
+      console.log('error:', error)
 
+      // TODO extract the common codes and statuses into a shared file that can be easily used everywhere instead of retyping them and inventing the wheel again
+      response.status(502).json({
+        code: 1000,
+        message: 'Error in database connection'
+      })
+    })
 })
 
 app.listen(config.app.port, () => {
