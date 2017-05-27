e7fa9faca01deeae9bdc85975484373709cf840f
Mika Turunen <mika.turunen@ymail.com>
2017-05-28 00:36:51 +0300

    Final fixes for shared template files.

--- api/generate-files-from-templates.sh
+++ api/generate-files-from-templates.sh
@@ -4,12 +4,9 @@ TEMPLATES="./docker-templates"
 PSP="./psp"
 
 # Create the Dockerfile from node dockerfile base template for payments
-cat $TEMPLATES/Dockerfile-node-shared $PSP/Dockerfile-payments > $PSP/Dockerfile
+cat $TEMPLATES/Dockerfile-node-shared $PSP/Dockerfile-psp > $PSP/Dockerfile
 echo "Generated Dockerfile for payments API."
-# Copies the node shared configuration for the API
-cat node-api.yml > $PSP/source/config/app.yml
-echo "Copied node-api.yml to psp API config/app.yml"
 
 # Create production ready docker-compose.yml
-cat $TEMPLATES/docker-compose-shared.yml $PSP/docker-compose-payments.yml > docker-compose.yml
+cat $TEMPLATES/docker-compose-shared.yml $PSP/docker-compose-psp.yml > docker-compose.yml
 echo "Generated docker-compose.yml for all APIs."
