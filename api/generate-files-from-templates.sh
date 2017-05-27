b7e48f599b395c1a8031fe75080a06cdbc632a7e
Mika Turunen <mika.turunen@ymail.com>
2017-05-28 00:32:36 +0300

    Update generator file variable names.

--- api/generate-files-from-templates.sh
+++ api/generate-files-from-templates.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
 TEMPLATES="./docker-templates"
-PAYMENTS="./psp"
-MERCHANT="./merchant"
+PSP="./psp"
 
 # Create the Dockerfile from node dockerfile base template for payments
-cat $TEMPLATES/Dockerfile-node-shared $PAYMENTS/Dockerfile-payments > $PAYMENTS/Dockerfile
+cat $TEMPLATES/Dockerfile-node-shared $PSP/Dockerfile-payments > $PSP/Dockerfile
 echo "Generated Dockerfile for payments API."
 # Copies the node shared configuration for the API
-cat node-api.yml > $PAYMENTS/source/config/app.yml
+cat node-api.yml > $PSP/source/config/app.yml
 echo "Copied node-api.yml to psp API config/app.yml"
 
 # Create production ready docker-compose.yml
-cat $TEMPLATES/docker-compose-shared.yml $PAYMENTS/docker-compose-payments.yml > docker-compose.yml
+cat $TEMPLATES/docker-compose-shared.yml $PSP/docker-compose-payments.yml > docker-compose.yml
 echo "Generated docker-compose.yml for all APIs."
