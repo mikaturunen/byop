42fdaa4470168c24ca600dc0a8e492838b31070f
Mika Turunen <mika.turunen@ymail.com>
2017-07-30 10:55:37 +0300

    Fix swagger definition for payment and match the mock json to it.

--- api/api/tests/legacy-payment.js
+++ api/api/tests/legacy-payment.js
@@ -10,64 +10,26 @@ chai.use(chaiHttp)
 process.env.NODE_ENV = 'test'
 
 describe('Legacy payment wrapper', () => {
-  describe('POST /api/v1/overlay/:merchantId/payment/open/single', _ => {
+  describe('POST /v1/payment/open/single', _ => {
     it('with empty payment', done => {
       const payment = {
       }
 
       chai
         .request(server)
-        .post('/api/v1/overlay/12345678/payment/open/single')
+        .post('/v1/payment/open/single')
         .send(payment)
         .end((error, response) => {
-          response.status.should.eql(502)
-          response.body.should.be.a('object')
-          response.body.should.have.property('code').eql('xxxx')
-          response.body.should.have.property('message').eql('We have no idea what went wrong, we are truly sorry for this, please get in touch with our developers at `...`.')
-
+          response.status.should.eql(400)
           done()
         })
     })
 
     it('with a valid payment', done => {
-      let body = {
-        payment: {
-          totalAmount: 1000,
-          reference: 'string',
-          stamp: 'string',
-          items: [{
-            reference: '12344',
-            stamp: '11111111',
-            amount: 1000,
-            description: '',
-            categoryCode: '12332',
-            deliveryDate: '20170602',
-          }],
-          content: 1,
-          language: 'FI',
-          country: 'FIN',
-          currency: 'EUR',
-          message: '',
-          customer: {
-            firstName: 'Keijo',
-            lastName: 'Romanof',
-            email: 'matti@couch.io'
-          },
-          redirect: {
-            return:  'http://demo1.checkout.fi/xml2.php?test=1',
-            cancel:  'http://demo1.checkout.fi/xml2.php?test=2',
-            reject:  'http://demo1.checkout.fi/xml2.php?test=3',
-            delayed: 'http://demo1.checkout.fi/xml2.php?test=4'
-          },
-          address: {
-            postalCode: '00100',
-            streetAddress: 'Katutie 12',
-            city: 'Helsinki',
-            country: 'Finland'
-          }
-        },
-        hmac: 'NOT CALCULATED'
-      }
+      let body = require('../src/api/mocks/payment.json')
+
+      console.log('body to post')
+      console.log(JSON.stringify(body, null,2 ))
 
       body.hmac = crypto
        .createHmac('sha256', 'SAIPPUAKAUPPIAS')
@@ -77,7 +39,8 @@ describe('Legacy payment wrapper', () => {
 
       chai
         .request(server)
-        .post('/api/v1/overlay/375917/payment/open/single')
+        .post('/v1/payment/open/single')
+        .set('Accepts', 'application/json')
         .send(body)
         .end((error, response) => {
           response.status.should.eql(200)
