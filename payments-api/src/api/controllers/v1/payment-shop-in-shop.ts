83ca9839d14cf5f2871f3d4ac09b1e83d0e1534d
Mika Turunen <mika.turunen@ymail.com>
2017-07-31 12:11:16 +0300

    Update swagger.yaml and the v1 controlle for sis cases.

--- payments-api/src/api/controllers/v1/payment-shop-in-shop.ts
+++ payments-api/src/api/controllers/v1/payment-shop-in-shop.ts
@@ -1,7 +1,9 @@
 
-import { OpenPayment, ClientError, PaymentWall } from '../shared-types'
-import { clientErrors, serverErrors } from '../errors'
+import { OpenPayment, ClientError, PaymentWall } from '../../../shared-types'
+import { clientErrors, serverErrors } from '../../../errors'
+import { preparePayment } from '../../../payment-preparation'
 
+import * as express from 'express'
 import * as crypto from 'crypto'
 import * as unirest from 'unirest'
 import * as xml2js from 'xml2js'
@@ -216,4 +218,45 @@ export const v1SpecificValidations = (payment: LegacyOpenPaymentSis) => new Prom
  */
 export const sendLegacyPayment = (openPayment: LegacyOpenPaymentSis) => {
   return openPaymentWall(openPayment)
+}
+
+// TODO remove completel once we are done with tests, this is not required at all.
+const SECRET = 'SAIPPUAKAUPPIAS'
+
+/**
+ * Express rest api handler for v1 shop-in-shop payments. Wraps the payment into a much more convenient format that is more user friendly to use.
+ * Follows exactly the same object schema as the v2. Just transformed into v1 and most of the content is ignored if it's not present in v1.
+ *
+ * @param {express.Request} request Express request
+ * @param {express.Response} response Express response
+ */
+export const openPayment = (request: express.Request, response: express.Response) => {
+  log.info(`start openPayment for shop-in-shop`)
+    
+  const openPayment: OpenPayment = request.body.payment
+  const merchantId: string = request.body.merchantId
+  const clientHmac: string = request.body.hmac
+  // TODO: read secret from db for merchant
+  const merchantSecret = SECRET
+  
+  log.info(`start openPayment for shop-in-shop, merchant ${merchantId}`)
+
+  preparePayment(merchantId, merchantSecret, clientHmac, openPayment)
+    .then(paymentSet => createLegacyOpenPayment(paymentSet.merchantId, paymentSet.merchantSecret, paymentSet.payment))
+    .then(payment => v1SpecificValidations(payment))
+    .then(payment => sendLegacyPayment(payment))
+    .then(paymentWall => {
+      response.json(paymentWall)
+      log.info(`end openPayment for shop-in-shop, OK`)
+    })
+    .catch((error: ClientError) => {
+      if (!error.http || !error.code) {
+        log.error(`Unhandled error in openPayment for shop-in-shop: `, error)
+        // TODO list this error into an alarm list that the developers will follow and fix as these emerge
+        response.status(502).json(serverErrors.general)
+      } else {
+        log.error(`Error in openPayment for shop-in-shop: `, error)
+        response.status(error.http).json(error)
+      }
+    })
 }
