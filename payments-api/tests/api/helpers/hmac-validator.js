8e4a01fac95e64820fa71c5d3085f7335753707f
Mika Turunen <mika.turunen@ymail.com>
2017-08-08 13:15:32 +0300

    Write tests for hmac validator

--- payments-api/tests/api/helpers/hmac-validator.js
+++ payments-api/tests/api/helpers/hmac-validator.js
@@ -1,21 +1,56 @@
 const chai = require('chai')
 const fs = require('fs')
-const path = require('path')
+const crypto = require('crypto')
 
 const should = chai.should()
 const expect = chai.expect
 const assert = chai.assert
-const transformer = require('../../../build/api/helpers/transform-legacy-xml-to-json').default
+const isHmacValid = require('../../../build/api/helpers/hmac-validator').default
 
-const mockXmlLocation = path.join(__dirname, '../../../src/api/mocks/example-xml.xml')
 process.env.NODE_ENV = 'test'
 
 describe('Helper hmac validation', () => {
-  describe('Returns', _ => {
-    it('True when the validation matches on sha256', _ => {
+  const merchantSecret = 'magic-unicorns-of-fantasy-shoot-fire-and-steam-from-their-eyes'
+
+  describe('True when', _ => {
+    let clientHmac
+    let payload = 'testing+foo+bar'
+
+    before(done => {
+      clientHmac = crypto
+        .createHmac('sha256', merchantSecret)
+        .update(new Buffer(JSON.stringify(payload)).toString('base64'))
+        .digest('hex')
+        .toUpperCase()
+      done()
     })
 
-    it('False when the validation does not match on sha256', _ => {
+    it('Validation matches on sha256 with correct merchant shared secret', () => assert.isTrue(
+      isHmacValid(clientHmac, merchantSecret, payload)
+    ))
+  })
+
+  describe('False when the validation', () => {
+    let clientHmac
+    let payload = 'testing+foo+bar'
+    let wrongPayload = 'testing-foo-bar'
+    let wrongMerchantSecret = 'magic-unicorns-of-fantasy-do-not-shoot-fire-and-steam-from-their-eyes'
+
+    before(done => {
+      clientHmac = crypto
+        .createHmac('sha256', merchantSecret)
+        .update(new Buffer(JSON.stringify(payload)).toString('base64'))
+        .digest('hex')
+        .toUpperCase()
+      done()
     })
+
+    it('Does not match on sha256 with incorrect merchant shared secret', () => assert.isFalse(
+      isHmacValid(clientHmac, wrongMerchantSecret, payload)
+    ))
+
+    it('Does not match on sha256 with incorrect payload', () => assert.isFalse(
+      isHmacValid(clientHmac, merchantSecret, wrongPayload)
+    ))
   })
 })
